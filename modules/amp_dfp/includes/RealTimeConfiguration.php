bea65bdeaf01f741e3e86bb03902f82b51024a50
James Williams <jimwills@hotmail.com>
2018-12-11 08:39:57 +0000

    DD-733 review fixes

--- modules/amp_dfp/includes/RealTimeConfiguration.php
+++ modules/amp_dfp/includes/RealTimeConfiguration.php
@@ -1,5 +1,9 @@
 <?php
 
+/**
+ *  Implements JsonSerializable Interface
+ */
+
 class RealTimeConfiguration implements JsonSerializable {
   
   private $vendors;
@@ -12,38 +16,65 @@ class RealTimeConfiguration implements JsonSerializable {
     $this->timeout = $timeout;
   }
   
+  /**
+   * 
+   * @return $this->vendors
+   */
   public function vendors() {
     return $this->vendors;
   }
   
+  /**
+   * 
+   * @return $this->urls
+   */
   public function urls() {
     return $this->urls;
   }
   
+  /**
+   * 
+   * @return $this->timeout
+   */
   public function timeout() {
     return $this->timeout;
   }
   
+  /**
+   * 
+   * @param type $vendors
+   * @return type array
+   */
   private function formatVendor($vendors) {
     $formatted = array();
-    for($i= 0; $i < count($vendors); $i++) {
+    for ($i= 0; $i < count($vendors); $i++) {
       $formatted[$vendors[$i]['vendor_name']] = $this->formatMacros($vendors[$i]['macros']);
     }
     
     return $formatted;
   }
   
+  /**
+   * 
+   * @param type $macros
+   * @return type array
+   */
   private function formatMacros($macros) {
     $formatted = array();
-    for($i= 0; $i < count($macros); $i++) {
+    for ($i= 0; $i < count($macros); $i++) {
       $formatted[$macros[$i]['macro']] = $macros[$i]['value'] ;
     }
     return $formatted;
   }
-   
+  
+  /**
+   * 
+   * @param type $urls
+   * @return type array
+   */
   private function formatUrls($urls) {
     $formatted = array();
-    for($i=0; $i < count($urls); $i++) {
+    for ($i=0; $i < count($urls); $i++) {
       if(!empty($urls[$i]['rtc_error_url'])) {
         $formatted[] = (object) [
           'url' => $urls[$i]['rtc_url'],
@@ -56,6 +87,10 @@ class RealTimeConfiguration implements JsonSerializable {
     return $formatted;
   }
   
+  /**
+   * 
+   * @return type json object
+   */
   public function jsonSerialize() {
     return array(
     'vendors' => $this->vendors,
